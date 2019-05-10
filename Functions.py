f4796ee290d58e0952216a8d92a858418b133da7
FCS <rohin.tunikipati@planet1world.com>
2019-05-10 15:09:51 +0800

    Functions

---
+++ Functions.py
@@ -0,0 +1,16 @@
+# Define function names()
+def names():
+    # Set up name variable with input
+    name = str(input('Enter your name: '))
+    # Check whether name has a vowel
+    if set('aeiou').intersection(name.lower()):
+        print('Your name contains a vowel.')
+    else:
+        print('Your name does not contain a vowel.')
+
+    # Iterate over name
+    for letter in name:
+        print(letter)
+
+# Call the function
+names()
